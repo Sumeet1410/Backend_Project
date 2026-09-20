cdb669e374713b5b2ec8cf226ea88b8a2b71e771
Sumeet1410 <sumeetthakkar1410@gmail.com>
2026-09-20 16:45:36 +0530

    Minor fixes

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,6 +1,7 @@
 import {createClient} from "redis";
-const redisClient=createClient({
-    url:process.env.REDIS_URL,
+const redisClient = createClient({
+    url: process.env.REDIS_URL,
+    pingInterval: 1000 * 30, // PING every 30s to prevent Upstash from closing idle connection
 });
 redisClient.on("error",(err)=>{
     console.log("Redis error",err);
